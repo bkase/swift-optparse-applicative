648616abfdc9449a76a98249d0b74557fd333df9
Brandon Kase <brandon.kase@gmail.com>
2017-05-07 00:08:22 -0700

    Attempted a protocol-based Parser definition
    Seems not possible this way though

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -30,16 +30,44 @@ struct OptProperties<A> {
     let metaVar: String
     let showDefault: String?
 }
-struct Opt<A> {
+
+
+/* A shitty approximation of a GADT */
+protocol Parser {
+    associatedtype Value
+}
+extension Parser {
+    func map<B, PB: Parser>(_ f: (Value) -> B) -> PB
+        where PB.Value == B {
+            fatalError("This may not be possible?")
+        /*if let n = self as? Nil<Value> {
+            return Nil<B>(a: n.a.map(f))
+        }*/
+    }
+}
+struct Nil<A>: Parser/*<A>*/ {
+    typealias Value = A
+    let a: A?
+}
+struct Opt<A>: Parser/*<A>*/ {
+    typealias Value = A
     let main: OptReader<A>
     let metadata: OptProperties<A>
 }
-
-indirect enum Parser<A> {
-    case Nil
-    case Opt(fa: Opt<A>)
-    case Alt(p1: Parser<A>, p2: Parser<A>)
-    // TODO: How to represent this?
-    /* case Mult<A, U>(p1: Parser<(U) -> A>, p2: Parser<U>)
-    case Bind(p: Parser<U>, f: (U) -> Parser<A>) */
+struct Alt<A, P: Parser>: Parser/*<A>*/ where P.Value == A {
+    typealias Value = A
+    let p1: P
+    let p2: P
+}
+struct Mult<A, B, PAB: Parser, PA: Parser>: Parser/*<B>*/
+    where PAB.Value == (A) -> B, PA.Value == A {
+    typealias Value = B
+    let p1: PAB
+    let p2: PA
+}
+struct Bind<A, B, PA: Parser, PB: Parser>: Parser/*<B>*/
+    where PA.Value == A, PB.Value == B {
+    typealias Value = B
+    let p: PA
+    let f: (A) -> PB
 }
